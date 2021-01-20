f7dc1084ad44124126a31e891275c1976d4e91a7
mertcankose <mertcankose97@outlook.com>
2021-01-20 20:32:51 +0300

    ecourse added

--- src/components/Works/workMixin.js
+++ src/components/Works/workMixin.js
@@ -4,12 +4,20 @@ export const workMixin = {
       works: [
         {
           siteLink: "https://vue-admin-dashboard.mertcankose.vercel.app/",
+          photo: "e-course-dashboard",
+          heading: "E-Course Dashboard",
+          description:
+            "An e-course dashboard that I developed with vue, you can sign up and then sign in the system. I also used Tailwind Css in the project.",
+          githubLink: "https://github.com/mertcankose/E-Course-Dashboard",
+          technologies: ["JavaScript", "Vue", "Firebase", "Tailwind Css"],
+        },
+        {
+          siteLink: "https://vue-admin-dashboard.mertcankose.vercel.app/",
           photo: "vue-admin-panel",
           heading: "Vue Admin Panel",
-          description:
-            "An admin panel dashboard that I developed with Vue. Also I used Vuesax library in the project.",
+          description: "An admin panel dashboard that I developed with Vue. Also I used Vuesax library in the project.",
           githubLink: "https://github.com/mertcankose/Vue-Admin-Dashboard",
-          technologies: ["JavaScript", "Vue", "Vuesax"]
+          technologies: ["JavaScript", "Vue", "Vuesax"],
         },
         {
           siteLink: "https://react-admin-dashboard.vercel.app/",
@@ -17,7 +25,7 @@ export const workMixin = {
           heading: "React Admin Panel",
           description: "An admin panel dashboard that I developed with React.",
           githubLink: "https://github.com/mertcankose/React-Admin-Dashboard",
-          technologies: ["JavaScript", "React"]
+          technologies: ["JavaScript", "React"],
         },
         {
           siteLink: "https://tranquil-sierra-90329.herokuapp.com/",
@@ -26,16 +34,15 @@ export const workMixin = {
           description:
             "A todo list app that I developed with Express.js. Also I used MongoDB as database and I used Ejs as view engine.",
           githubLink: "https://github.com/mertcankose/TodoList",
-          technologies: ["Express.js", "MongoDB", "Ejs"]
+          technologies: ["Express.js", "MongoDB", "Ejs"],
         },
         {
           siteLink: "https://thirsty-poitras-8a4c99.netlify.app/",
           photo: "monster-hunt",
           heading: "Monster Hunt",
-          description:
-            "A little game that I developed with Vue.js. This is the first app on Vue.js.",
+          description: "A little game that I developed with Vue.js. This is the first app on Vue.js.",
           githubLink: "https://github.com/mertcankose/Monster-Hunt",
-          technologies: ["JavaScript", "Vue"]
+          technologies: ["JavaScript", "Vue"],
         },
         {
           siteLink: "https://mailchimp-newsletter-mertcan.herokuapp.com",
@@ -44,7 +51,7 @@ export const workMixin = {
           description:
             "A newsletter that I developed with Express.js. If you send email to me, you can use this alternative :)",
           githubLink: "https://github.com/mertcankose/Mailchimp-Newsletter",
-          technologies: ["Node.js", "Mailchimp API"]
+          technologies: ["Node.js", "Mailchimp API"],
         },
         {
           siteLink: "https://weather-app-mertcan.herokuapp.com/",
@@ -53,16 +60,15 @@ export const workMixin = {
           description:
             "A weather app that I developed with Node.js and Express.js. Also I used Open Weather Map Apı for weather datas.",
           githubLink: "https://github.com/mertcankose/Weather-App",
-          technologies: ["Express.js", "Open Weather API"]
+          technologies: ["Express.js", "Open Weather API"],
         },
         {
           siteLink: "https://github.com/mertcankose/Daily-Frontend",
           photo: "daily-frontend",
           heading: "Daily Frontend",
-          description:
-            "The Project that I am coding the designs I like. I have used a lot of Scss.",
+          description: "The Project that I am coding the designs I like. I have used a lot of Scss.",
           githubLink: "https://github.com/mertcankose/Daily-Frontend",
-          technologies: ["Css", "Scss", "Gulp.js"]
+          technologies: ["Css", "Scss", "Gulp.js"],
         },
         {
           siteLink: "https://confident-bose-ffea0e.netlify.app/",
@@ -71,7 +77,7 @@ export const workMixin = {
           description:
             "A landing page that I developed Scss and a little bit JavaScript. This is the first serious website. It was funny for me :)",
           githubLink: "https://github.com/mertcankose/manage-landing-page-",
-          technologies: ["Scss", "JavaScript", "Gulp.js"]
+          technologies: ["Scss", "JavaScript", "Gulp.js"],
         },
         {
           siteLink: "https://simongamemertcan.netlify.app/",
@@ -80,9 +86,9 @@ export const workMixin = {
           description:
             "A game that I developed with JavaScript. This project 	strengthened my knowledge of JavaScript a lot. ",
           githubLink: "https://github.com/mertcankose/TheSimonGame",
-          technologies: ["Css", "JavaScript"]
-        }
-      ]
+          technologies: ["Css", "JavaScript"],
+        },
+      ],
     };
-  }
+  },
 };
